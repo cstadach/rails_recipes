75cf034e597fa019e6af572b0a804276c1aac715
Christian Stadach <christian.stadach@otto.de>
2023-06-10 13:53:21 +0200

    added useEffect to get recipe data from server

--- rails_react_recipe/app/javascript/components/Recipe.jsx
+++ rails_react_recipe/app/javascript/components/Recipe.jsx
@@ -5,6 +5,20 @@ const Recipe = () => {
   const params = useParams();
   const navigate = useNavigate();
   const [recipe, setRecipe] = useState({ ingredients: "" });
+
+  // grabbing recipe data from server
+  useEffect(() => {
+    const url = `/api/v1/show/${params.id}`;
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setRecipe(response))
+      .catch(() => navigate("/recipes"));
+  }, [params.id]);
 };
 
 export default Recipe;
